702709f856736b3549d4c39666b7943cacea595b
ilya <ilasafronov96@gmail.com>
2023-07-25 23:22:51 +0300

    added screenshot system

--- src/main/java/org/resk/system/Main.java
+++ src/main/java/org/resk/system/Main.java
@@ -6,6 +6,9 @@ import org.resk.units.Point;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
@@ -91,6 +94,8 @@ public class Main extends Canvas
     }
     private void init() {
         frame.setResizable(false);
+        this.addMouseMotionListener(new ScreenshotsAdapter(renderer));
+        this.addMouseListener(new ScreenshotsAdapter(renderer));
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -106,7 +111,7 @@ public class Main extends Canvas
         Point start_point = new Point(1, 0, 0,new LorenzPoint(main.renderer));
 
         while (main.running){
-            Point new_point = start_point.getNext(0.0001);
+            Point new_point = start_point.getNext(0.001);
             //System.out.println(new_point);
             new_point.draw();
             start_point = new_point;
